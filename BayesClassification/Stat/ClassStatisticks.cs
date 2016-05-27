cab377427c38b33fb8b882c102b12de54a513a98
Kiedro <kiedro@outlook.com>
2016-05-27 22:50:31 +0200

    ClassStatisticks - class apriori prob, features in class apriori prob

---
+++ BayesClassification/Stat/ClassStatisticks.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BayesClassification.Models;
+
+namespace BayesClassification.Stat
+{
+    public class ClassStatisticks
+    {
+        public Classification Class { get; set; }
+        public IList<Patient> Patients { get; set; }
+        public double ClassProbability { get; set; }
+        public Dictionary<int, double> FeaturesStatisticks = new Dictionary<int, double>();
+
+        public ClassStatisticks(IList<Patient> patients, Classification @class)
+        {
+            this.Patients = patients.Where(x => x.Classification == @class).ToList();
+            ClassProbability = (double)Patients.Count / patients.Count;
+            this.Class = @class;
+
+            CalculateFeaturesStatisticks();
+        }
+
+        private void CalculateFeaturesStatisticks()
+        {
+            int featuresCount = Patients.First().Features.Count;
+            IList<Feature> features = GetAllFeatures();
+
+            foreach (Feature feature in Patients.First().Features)
+            {
+                int i = feature.Id;
+                if (Patients.First().Features.First(x => x.Id == i).Type == FeatureType.Binary)
+                {
+                    double featureProbability = features.Where(x => x.Id == i).Sum(x => x.Value) / Patients.Count;
+                    FeaturesStatisticks.Add(i, featureProbability);
+                }
+            }
+        }
+
+        private IList<Feature> GetAllFeatures()
+        {
+            IList<Feature> features = new List<Feature>();
+            foreach (Patient patient in Patients)
+            {
+                foreach (Feature feature in patient.Features)
+                {
+                    features.Add(feature);
+                }
+            }
+
+            return features;
+        }
+    }
+}
---
+++ UnitTests/ClassStatisticksTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BayesClassification;
+using BayesClassification.Models;
+using BayesClassification.Stat;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ClassStatisticksTests
+    {
+        private IList<Patient> patients;
+        private IList<Feature> features;
+        List<Feature> features2;
+        public ClassStatisticksTests()
+        {
+            features = new List<Feature>
+            {
+                new Feature {Id = 2, Type  = FeatureType.Binary, Value = 1},
+                new Feature {Id = 3, Type  = FeatureType.Binary, Value = 0},
+                new Feature {Id = 4, Type  = FeatureType.Binary, Value = 1},
+            };
+            features2 = new List<Feature>
+            {
+                new Feature {Id = 2, Type = FeatureType.Binary, Value = 0},
+                new Feature {Id = 3, Type = FeatureType.Binary, Value = 1},
+                new Feature {Id = 4, Type = FeatureType.Binary, Value = 0},
+            };
+
+            patients = new List<Patient>
+            {
+                new Patient(Classification.Normal, features),
+                new Patient(Classification.Hyperfunction, features),
+                new Patient(Classification.Subnormal, features),
+                new Patient(Classification.Normal, features),
+                new Patient(Classification.Hyperfunction, features),
+                new Patient(Classification.Subnormal, features),
+                new Patient(Classification.Normal, features2),
+                new Patient(Classification.Hyperfunction, features2),
+                new Patient(Classification.Subnormal, features2),
+
+            };
+        }
+
+        [Fact]
+        public void CreateClassStatisticks_PacientCollections_APrioriStat()
+        {
+            var classStat = new ClassStatisticks(patients, Classification.Normal);
+
+            Assert.Equal(0.333, classStat.ClassProbability, 3);
+        }
+
+        [Fact]
+        public void CreateClassStatisticks_PacientCollectionWithBinaryFeatures_FeaturesStas()
+        {
+            var classStat = new ClassStatisticks(patients, Classification.Normal);
+
+            Assert.Equal(0.667, classStat.FeaturesStatisticks[2], 3);
+        }
+    }
+}
